8f043a82d11db6f58f84779bfc9d8958f5055613
SK <68657125+skeshta@users.noreply.github.com>
2022-07-08 16:23:44 +0200

    Added the first test. Added enough code to pass that test.

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,9 @@
+public class BowlingGame {
+    private int score = 0;
+
+    //Get score
+    public int getScore() {
+        return score;
+    }
+
+}
---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Initial score is 0")
+    void TestInitialScore() {
+        assertEquals(0, game.getScore());
+    }
+}
