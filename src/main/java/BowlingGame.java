94477b671016d811784c71727310809dfedce09c
SK <68657125+skeshta@users.noreply.github.com>
2022-07-08 20:16:46 +0200

    Simplified code (hopefully). Passes.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -6,9 +6,15 @@ public class BowlingGame {
         return gameScore;
     }
 
+    public boolean checkInput(int rollOne, int rollTwo) {
+        boolean inputOK;
+        inputOK = rollOne + rollTwo <= 10 && Math.min(rollOne, rollTwo) >= 0;
+        return inputOK;
+    }
+
     public int nextFrame(int rollOne, int rollTwo) {
         int error_value = -2147483648;
-        if (rollOne + rollTwo > 10 || Math.min(rollOne, rollTwo) < 0) {
+        if (!checkInput(rollOne, rollTwo)) {
             return error_value;
         }
         else {
