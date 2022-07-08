b083f50a0244c31a553bd234a3b4437d4c77dc7b
SK <68657125+skeshta@users.noreply.github.com>
2022-07-08 20:53:34 +0200

    Adjusted code. Passes.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,13 +1,25 @@
 public class BowlingGame {
+    private final int error_value = -2147483648;
 
-    public boolean frameValid(int rollOne, int rollTwo) {
+    private boolean frameValid(int rollOne, int rollTwo) {
         boolean bothRollsOK;
         bothRollsOK = rollOne + rollTwo <= 10 && Math.min(rollOne, rollTwo) >= 0;
         return bothRollsOK;
     }
 
+    private boolean gameValid(int[] frames) {
+        boolean allFramesOK;
+        allFramesOK = (frames.length % 2 == 0);
+        return allFramesOK;
+    }
+
     public int scoreGameTotal(int[] frames) {
         int gameScore = 0;
+
+        if (!gameValid(frames)) {
+            return error_value;
+        }
+
         int totalRolls = frames.length;
         for (int i=0; i<totalRolls; i+=2) {
             gameScore += scoreSingleFrame(frames[i], frames[i+1]);
@@ -16,7 +28,6 @@ public class BowlingGame {
     }
 
     public int scoreSingleFrame(int rollOne, int rollTwo) {
-        int error_value = -2147483648;
         int frameScore = 0;
         if (!frameValid(rollOne, rollTwo)) {
             return error_value;
