420d75537d2b98375c2bb294409b1ba03529f841
SK <68657125+skeshta@users.noreply.github.com>
2022-07-08 20:09:15 +0200

    Added enough code to pass.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -8,7 +8,7 @@ public class BowlingGame {
 
     public int nextFrame(int rollOne, int rollTwo) {
         int error_value = -2147483648;
-        if (rollOne + rollTwo > 10) {
+        if (rollOne + rollTwo > 10 || Math.min(rollOne, rollTwo) < 0) {
             return error_value;
         }
         else {
