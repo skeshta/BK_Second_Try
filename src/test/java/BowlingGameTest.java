51cf4c35f8b5b4002d9cb772fa1b55db703fe79a
SK <68657125+skeshta@users.noreply.github.com>
2022-07-10 17:00:13 +0200

    Added a test to sum 10 regular frames (passes). Added a test to sum a spare and a regular frame (does not pass).

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -28,12 +28,37 @@ class BowlingGameTest {
 
     @Test
     @DisplayName("Two regular frames sum correctly")
-    void TestSumTwoRegularFrames() {
+    void TestSum2RegularFrames() {
         BowlingFrame[] frames = new BowlingFrame[2];
         frames[0] = new BowlingFrame();
         frames[1] = new BowlingFrame();
         frames[0].set(2, 5);
         frames[1].set(3, 6);
         assertEquals(16, game.getScore(frames));
+    }
+
+    @Test
+    @DisplayName("Ten regular frames sum correctly")
+    void TestSum10RegularFrames () {
+        int rollOne = 2;
+        int rollTwo = 3;
+        BowlingFrame[] frames = new BowlingFrame[10];
+        BowlingFrame regularFrame = new BowlingFrame();
+        regularFrame.set(rollOne, rollTwo);
+        for (int count = 0; count < 10; count++) {
+            frames[count] = regularFrame;
+        }
+        assertEquals(50, game.getScore(frames));
+    }
+
+    @Test
+    @DisplayName("Spare sums correctly")
+    void TestSpareSumsCorrectly () {
+        BowlingFrame[] frames = new BowlingFrame[2];
+        BowlingFrame spare = new BowlingFrame();
+        BowlingFrame regularFrame = new BowlingFrame();
+        spare.set(1, 9);
+        regularFrame.set(2, 3);
+        assertEquals(17, game.getScore(frames));
     }
 }
