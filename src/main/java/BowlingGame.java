fbd061dfcb678fea3ba801d79bb1be7a0ebc69de
SK <68657125+skeshta@users.noreply.github.com>
2022-07-10 16:34:25 +0200

    Test passes.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,6 +2,13 @@ public class BowlingGame {
 
     public int getScore(BowlingFrame[] frames) {
         int score = 0;
+        int numberOfFrames;
+        int currentScore;
+        numberOfFrames = frames.length;
+        for (int count = 0; count < numberOfFrames; count++) {
+            currentScore = frames[count].sumRolls();
+            score += currentScore;
+        }
         return score;
     }
 
